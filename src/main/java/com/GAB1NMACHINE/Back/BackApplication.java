16d648dd872e30f2829a9893e857ffbd3c2d6376
Paul Preuvost <118807111+PaulPreuvost@users.noreply.github.com>
2024-05-16 13:40:22 +0200

    push

---
+++ src/main/java/com/GAB1NMACHINE/Back/BackApplication.java
@@ -0,0 +1,13 @@
+package com.GAB1NMACHINE.Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackApplication.class, args);
+	}
+
+}
