defaba345f8fd3726ba3a9f19189f6dda4066d99
Alex2eme <alex.gontier@supinfo.com>
2024-05-17 10:18:32 +0200

    Create .gitignore

--- src/main/java/com/gab1machine/fridge/user/UserController.java
+++ src/main/java/com/gab1machine/fridge/user/UserController.java
@@ -1,4 +1,28 @@
 package com.gab1machine.fridge.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@RestController
 public class UserController {
+
+    private final UserServices userServices;
+
+    @PostMapping
+    public @ResponseBody UserDto createUser(@RequestBody UserDto season) {
+        return this.userServices.createUser(season);
+    }
+
+    @GetMapping
+    public @ResponseBody ResponseEntity<UserDto> getUser(@RequestParam(name = "id") UUID id) {
+        Optional<UserDto> dto = this.userServices.getUser(id);
+        return dto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/gab1machine/fridge/user/UserServices.java
+++ src/main/java/com/gab1machine/fridge/user/UserServices.java
@@ -3,6 +3,7 @@ package com.gab1machine.fridge.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
@@ -13,6 +14,10 @@ public class UserServices {
 
     private final UserRepository userRepository;
 
+    private UserDto entityToDto(UserEntity entity) {
+        return new UserDto(entity.getId(), entity.getName());
+    }
+
     public UserDto createUser(UserDto user) {
         UUID id = UUID.randomUUID();
         UserEntity userEntity = new UserEntity();
@@ -20,5 +25,10 @@ public class UserServices {
         userEntity.setName(user.name());
         UserEntity savedUser = this.userRepository.save(userEntity);
         return new UserDto(savedUser.getId(), savedUser.getName());
+    }
+
+    public Optional<UserDto> getUser(UUID id) {
+        Optional<UserEntity> entity = this.userRepository.findById(id);
+        return entity.map(this::entityToDto);
     }
 }
