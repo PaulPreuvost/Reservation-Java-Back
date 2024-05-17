77888bb3de195b1714455220d4ab4a5a033aeca2
Ferreira-Orlann <118378969+Sora-u@users.noreply.github.com>
2024-05-17 11:22:46 +0200

    Documentation Start and POST /reservation doc

--- src/main/java/com/gab1machine/fridge/reservation/ReservationController.java
+++ src/main/java/com/gab1machine/fridge/reservation/ReservationController.java
@@ -1,6 +1,11 @@
 package com.gab1machine.fridge.reservation;
 
 import com.gab1machine.fridge.storage.StorageDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +25,17 @@ public class ReservationController {
         return dto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Crate a Reservation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Created successfully",
+                content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ReservationDto.class))
+            }),
+            @ApiResponse(responseCode = "200", description = "Invalid Informations (date or storage invalid)",
+                    content = @Content
+            ),
+    })
     @PostMapping
     public @ResponseBody ResponseEntity<ReservationDto> postStorage(@RequestBody ReservationDto requestDto) {
         Optional<ReservationDto> dto = this.reservationServices.createReservation(requestDto);
